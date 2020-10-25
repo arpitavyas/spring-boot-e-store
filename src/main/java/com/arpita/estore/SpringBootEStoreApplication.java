504838244f0921e9d1397c8783b432a0fadb9bfa
arpita vyas <arpitavyas@192-168-1-108.tpgi.com.au>
2020-10-25 11:15:05 +1100

    files commit

---
+++ src/main/java/com/arpita/estore/SpringBootEStoreApplication.java
@@ -0,0 +1,13 @@
+package com.arpita.estore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootEStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootEStoreApplication.class, args);
+	}
+
+}
